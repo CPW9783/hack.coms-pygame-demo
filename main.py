28e4d4a8edff93ced3e3d81fce816618511469b6
Christopher <cpw9783@rit.edu>
2024-10-16 16:31:42 -0400

    controller support added

--- main.py
+++ main.py
@@ -3,6 +3,9 @@ import pygame
 # Initializes pygame
 pygame.init()
 
+# Initalizes the joystick module
+pygame.joystick.init()
+
 # Game clock for framerate
 clock = pygame.time.Clock()
 
@@ -29,6 +32,57 @@ while running:
         if event.type == pygame.QUIT:
             running = False
 
+        # detects if a controller is pluged in
+        if event.type == pygame.JOYDEVICEADDED:
+            print("Controller connected: " + str(event))
+            joy = pygame.joystick.Joystick(event.device_index)
+            # buttons = joy.get_numbuttons()
+            # print(screen, f"Number of buttons: {buttons}")
+
+            # for i in range(buttons):
+            #     button = joy.get_button(i)
+            #     print(screen, f"Button {i:>2} value: {button}")
+            # print(str(joy.get_name()))
+        
+        # movement with controller buttons
+        if event.type == pygame.JOYBUTTONDOWN:
+            print(event)
+            # X button
+            if event.button == 0:
+                playerY_change = -5
+            # A button
+            if event.button == 1:
+                playerX_change = 5
+            # B button
+            if event.button == 2:
+                playerY_change = 5
+            # Y button
+            if event.button == 3:
+                playerX_change = -5
+            # select button
+            if event.button == 8:
+                running = False
+
+        # Stops continuous movement when button is lifted (Controller)
+        if event.type == pygame.JOYBUTTONUP:
+            if event.button == 0 or event.button == 2:
+                playerY_change = 0
+            if event.button == 1 or event.button == 3:
+                playerX_change = 0
+
+
+        # D-pad Controls (Goofy setup)
+        if event.type == pygame.JOYAXISMOTION:
+            print(event)
+            if event.axis == 1 and event.value == 1:
+                playerX_change = 5
+            if event.axis == 1 and event.value <= -1:
+                playerX_change = -5
+            if event.axis == 4 and event.value == 1:
+                playerY_change = 5
+            if event.axis == 4 and event.value <= -1:
+                playerY_change = -5
+
         # Keyboard controls
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_UP:
@@ -40,6 +94,7 @@ while running:
             if event.key == pygame.K_LEFT:
                 playerX_change = -5
 
+        # Stops continuous movement when button is lifted (Keyboard)
         if event.type == pygame.KEYUP:
             if event.key == pygame.K_UP or event.key == pygame.K_DOWN:
                 playerY_change = 0
